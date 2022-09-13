b2d84fb1284229018a4c98f63e715d2801c74f15
liuzhuohao <804121985@qq.com>
2022-09-13 09:52:32 +0800

    add: add dockerfile docker image push

--- src/test/java/com/wanfeng/dingyuehao/DingyuehaoApplicationTests.java
+++ src/test/java/com/wanfeng/dingyuehao/DingyuehaoApplicationTests.java
@@ -1,16 +1,16 @@
-package com.wanfeng.dingyuehao;
-
-import com.wanfeng.dingyuehao.domain.Enum.PaymentEnum;
-import com.wanfeng.dingyuehao.domain.VO.PaymentReq;
-import com.wanfeng.dingyuehao.service.PaymentService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.math.BigDecimal;
-
-@SpringBootTest(classes = DingyuehaoApplication.class)
-class DingyuehaoApplicationTests {
-
-
-}
+//package com.wanfeng.dingyuehao;
+//
+//import com.wanfeng.dingyuehao.domain.Enum.PaymentEnum;
+//import com.wanfeng.dingyuehao.domain.VO.PaymentReq;
+//import com.wanfeng.dingyuehao.service.PaymentService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.math.BigDecimal;
+//
+//@SpringBootTest(classes = DingyuehaoApplication.class)
+//class DingyuehaoApplicationTests {
+//
+//
+//}
